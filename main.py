9040bea881ecbd06e1be2b618e66153fcd1e321e
Thomas Fare <thomasfare@thomass-mbp.mynetworksettings.com>
2024-06-17 20:18:01 -0400

    finished initial api

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request, Response
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
-
+app.config['CORS_HEADERS'] = 'application/json'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
@@ -26,6 +28,7 @@ def index():
 
 
 @app.route('/tickets')
+@cross_origin(supports_credentials=True)
 def get_tickets():
     tickets = Ticket.query.all()
     ticket_data = []
@@ -35,8 +38,38 @@ def get_tickets():
                 'description': ticket.description,
                 'status': ticket.status,
                 'email': ticket.email,
-                'subject': ticket.subject
+                'subject': ticket.subject,
+                'id': ticket.id
                 }
         ticket_data.append(data)
 
     return {'Tickets': ticket_data}
+
+
+@app.route('/submitTickets', methods=['POST'])
+@cross_origin(supports_credentials=True)
+def submit_tickets():
+    ticket = Ticket(name=request.json['name'], email=request.json['email'], description=request.json['description'], status=request.json['status'], subject=request.json['subject'])
+    db.session.add(ticket)
+    db.session.commit()
+    return Response("{'error':'false'}", status=200, mimetype='application/json')
+
+
+@app.route('/editTickets', methods=['PUT'])
+@cross_origin(supports_credentials=True)
+def edit_tickets():
+    ticket = Ticket.query.get(request.json['id'])
+    ticket.status = request.json['status']
+    db.session.commit()
+    return Response("{'error':'false'}", status=200, mimetype='application/json')
+
+
+@app.route('/deleteTickets', methods=['PUT'])
+@cross_origin(supports_credentials=True)
+def delete_tickets():
+    ticket = Ticket.query.get(request.json['id'])
+    # if ticket is None:
+    #     return Response("{'error':'true', 'message':'Selected ticket does not exist'}", status=400, mimetype='application/json')
+    db.session.delete(ticket)
+    db.session.commit()
+    return Response("{'error':'false'}", status=200, mimetype='application/json')
