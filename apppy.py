8d32a7c9082cc325daffa1f3715117203eb3ec42
Thomas Fare <thomasfare@thomass-mbp.mynetworksettings.com>
2024-07-03 13:50:02 -0400

    Fix vercel issue 4

--- apppy.py
+++ apppy.py
@@ -1,11 +1,15 @@
 from flask import Flask
 import os
+from flask_cors import CORS
 
 POSTRESQL_URL = os.environ.get('API_URL')
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'application/json'
 app.config['SQLALCHEMY_DATABASE_URI'] = POSTRESQL_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+
+cors = CORS(app)
 
 
 import controller
