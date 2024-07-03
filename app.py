c91c3e0cce5d21c2e6ed61066772f3ec0e8c1acc
Thomas Fare <thomasfare@thomass-mbp.mynetworksettings.com>
2024-07-03 14:48:11 -0400

    Finish api refactor

--- app.py
+++ app.py
@@ -7,5 +7,5 @@ POSTRESQL_URL = os.environ.get('API_URL')
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'application/json'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://neondb_owner:4qi5PguEWhol@ep-bitter-feather-a5gxsums.us-east-2.aws.neon.tech/neondb?sslmode=require'
+app.config['SQLALCHEMY_DATABASE_URI'] = POSTRESQL_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
