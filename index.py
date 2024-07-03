465597480992ff6b370d43061f634186c71e50d2
Thomas Fare <thomasfare@thomass-mbp.mynetworksettings.com>
2024-07-03 14:22:01 -0400

    Fix vercel

--- index.py
+++ index.py
@@ -5,11 +5,17 @@ from db import db
 from ticket import Ticket
 
 POSTRESQL_URL = os.environ.get('API_URL')
+
+
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'application/json'
 app.config['SQLALCHEMY_DATABASE_URI'] = POSTRESQL_URL
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 cors = CORS(app)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
